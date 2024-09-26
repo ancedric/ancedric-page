48ba2056c9d208b997e0263ad4ef816919ea2426
ancedric <101697896+ancedric@users.noreply.github.com>
2024-09-26 14:40:40 +0200

    Add of javascript file
    Update of the loader and add of javascript for the loader behaviour

---
+++ app.js
@@ -0,0 +1,8 @@
+function hideLoader(){
+    const loader = document.getElementById("loader");
+    const content = document.getElementById("content");
+    loader.style.display="none";
+    content.style.display = "block";
+}
+
+window.qddEventListener('load', hideLoader);
